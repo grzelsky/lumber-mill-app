33b2bf655bb2164b86bf36ffb7984ab2d0fc0f7b
grzelski <grzelaa@o2.pl>
2021-07-12 19:35:49 +0200

    Updates

---
+++ precache-manifest.bc074c19d3e65679145617b36b0b9d34.js
@@ -0,0 +1,98 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "49457090c80cda8b83b6bd17c6d9c105",
+    "url": "/lumber-mill-app/index.html"
+  },
+  {
+    "revision": "9a52c37a2387a3f05269",
+    "url": "/lumber-mill-app/static/css/main.fc05784f.chunk.css"
+  },
+  {
+    "revision": "d51292d77ae13adf30b9",
+    "url": "/lumber-mill-app/static/js/2.91beaa86.chunk.js"
+  },
+  {
+    "revision": "f231859d6585c4cd5f80c344783ed269",
+    "url": "/lumber-mill-app/static/js/2.91beaa86.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9a52c37a2387a3f05269",
+    "url": "/lumber-mill-app/static/js/main.37758d13.chunk.js"
+  },
+  {
+    "revision": "1b2f5f17fec665a2d729",
+    "url": "/lumber-mill-app/static/js/runtime-main.f36c46de.js"
+  },
+  {
+    "revision": "3f7e606a6f1cd3c8f24b0a6764e1f929",
+    "url": "/lumber-mill-app/static/media/background_1.3f7e606a.jpg"
+  },
+  {
+    "revision": "89eb32a4f189785dd884afc18110d032",
+    "url": "/lumber-mill-app/static/media/background_2.89eb32a4.jpg"
+  },
+  {
+    "revision": "4d9f5d99b246688089437c6d8501e720",
+    "url": "/lumber-mill-app/static/media/background_3.4d9f5d99.jpg"
+  },
+  {
+    "revision": "855e678932232161c3285b0f6dabbbde",
+    "url": "/lumber-mill-app/static/media/background_4.855e6789.jpg"
+  },
+  {
+    "revision": "5fc573ffde4cab925ee3b63d3c5b2f19",
+    "url": "/lumber-mill-app/static/media/background_5.5fc573ff.jpg"
+  },
+  {
+    "revision": "6cf09284c43e1ce6fafbd48aa0b101dd",
+    "url": "/lumber-mill-app/static/media/bale.6cf09284.jpg"
+  },
+  {
+    "revision": "0e05335970fad289a7473e35f8f08469",
+    "url": "/lumber-mill-app/static/media/buisness-woman.0e053359.jpg"
+  },
+  {
+    "revision": "984332a19da19478cf39c43e9dcfa489",
+    "url": "/lumber-mill-app/static/media/contact.984332a1.jpg"
+  },
+  {
+    "revision": "18e25b4622eed50c0d97827b7ced0bac",
+    "url": "/lumber-mill-app/static/media/deski.18e25b46.jpg"
+  },
+  {
+    "revision": "58a3a01a9426eb47eb43dd9edf3e9419",
+    "url": "/lumber-mill-app/static/media/granny.58a3a01a.jpg"
+  },
+  {
+    "revision": "6827dcdaf3e56f4be028785c5930d3d4",
+    "url": "/lumber-mill-app/static/media/kantowki.6827dcda.jpg"
+  },
+  {
+    "revision": "09d6a978b8ff5ef4389b8a13c4dde63b",
+    "url": "/lumber-mill-app/static/media/man.09d6a978.jpg"
+  },
+  {
+    "revision": "2fa4c668802b302252a0a4332dcd7a62",
+    "url": "/lumber-mill-app/static/media/woodworking.2fa4c668.png"
+  },
+  {
+    "revision": "ecca38f2ef36eca8a69c4d88517bc866",
+    "url": "/lumber-mill-app/static/media/worker_1.ecca38f2.jpg"
+  },
+  {
+    "revision": "bcbadf1a12ad6ed222afe1d18dc15178",
+    "url": "/lumber-mill-app/static/media/worker_2.bcbadf1a.jpg"
+  },
+  {
+    "revision": "7cc434281be7c03151def7f1ba24c149",
+    "url": "/lumber-mill-app/static/media/worker_3.7cc43428.jpg"
+  },
+  {
+    "revision": "f287fbd911b007429dc708d00ad26df6",
+    "url": "/lumber-mill-app/static/media/worker_4.f287fbd9.jpg"
+  },
+  {
+    "revision": "cbe92421a1668ccfcfd6d35ca2917dc6",
+    "url": "/lumber-mill-app/static/media/wycinka.cbe92421.jpg"
+  }
+]);
